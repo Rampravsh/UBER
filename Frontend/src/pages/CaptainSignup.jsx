b8ec61405b743a3de388df01932e8e8695ffd9f6
Rampravesh <rampraveshkr4545@gmail.com>
2026-04-18 13:50:22 +0530

    create captain signup page

---
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [vehicleColor, setVehicleColor] = useState("");
+  const [vehiclePlate, setVehiclePlate] = useState("");
+  const [vehicleCapacity, setVehicleCapacity] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
+  const [captainData, setCaptainData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCaptainData({
+      fullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+      vehicle: {
+        color: vehicleColor,
+        plate: vehiclePlate,
+        capacity: vehicleCapacity,
+        vehicleType: vehicleType,
+      },
+    });
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setVehicleColor("");
+    setVehiclePlate("");
+    setVehicleCapacity("");
+    setVehicleType("");
+  };
+
+  return (
+    <div className="py-5 px-5 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-20 mb-3"
+          src="https://www.svgrepo.com/show/505031/uber-driver.svg"
+          alt="Uber Driver Logo"
+        />
+
+        <form onSubmit={submitHandler}>
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's name
+          </h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's email
+          </h3>
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Vehicle Information</h3>
+          <div className="flex flex-col md:flex-row gap-4 mb-6">
+            <div className="w-full md:w-1/2 flex gap-2">
+              <input
+                required
+                className="bg-[#eeeeee] flex-grow rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+                type="text"
+                placeholder="Vehicle Color"
+                value={vehicleColor}
+                onChange={(e) => setVehicleColor(e.target.value)}
+              />
+              <input
+                type="color"
+                className="w-10 h-11 p-0 border-none bg-transparent cursor-pointer"
+                onChange={(e) => setVehicleColor(e.target.value)}
+                value={vehicleColor.startsWith("#") ? vehicleColor : "#000000"}
+              />
+            </div>
+            <input
+              required
+              className="bg-[#eeeeee] w-full md:w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Vehicle Plate"
+              value={vehiclePlate}
+              onChange={(e) => setVehiclePlate(e.target.value)}
+            />
+          </div>
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              max={4}
+              min={1}
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="number"
+              placeholder="Vehicle Capacity"
+              value={vehicleCapacity}
+              onChange={(e) => setVehicleCapacity(e.target.value)}
+            />
+            <select
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              value={vehicleType}
+              onChange={(e) => setVehicleType(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Vehicle Type
+              </option>
+              <option value="car">Car</option>
+              <option value="motorcycle">Motorcycle</option>
+              <option value="auto">Auto</option>
+            </select>
+          </div>
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg">
+            Create Captain Account
+          </button>
+        </form>
+        <p className="text-center">
+          Already have an account?{" "}
+          <Link to="/captain-login" className="text-blue-600">
+            Login here
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px] mt-6 leading-tight text-gray-500">
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service</span> apply.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
