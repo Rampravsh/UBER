335f1dc17262ff1d15461055a022fd37151bf300
Rampravesh <rampraveshkr4545@gmail.com>
2026-04-19 16:50:59 +0530

    create UserLogout.jsx

---
+++ Frontend/src/pages/UserLogout.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserLogout = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/logout`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          localStorage.removeItem("token");
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        navigate("/login");
+      });
+  }, [token, navigate]);
+
+  return <div>UserLogout</div>;
+};
+
+export default UserLogout;
