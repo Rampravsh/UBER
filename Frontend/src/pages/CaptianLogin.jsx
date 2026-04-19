74ab27bca9ba3d5f2d965a0246688f44ab2b2ae5
Rampravesh <rampraveshkr4545@gmail.com>
2026-04-19 16:48:22 +0530

    add captian login functionality

--- Frontend/src/pages/CaptianLogin.jsx
+++ Frontend/src/pages/CaptianLogin.jsx
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useCaptainData } from "../context/CaptainContext";
 
 const CaptianLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [captainData, setCaptainData] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+  const { setCaptain } = useCaptainData();
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setCaptainData({
+    const captain = {
       email: email,
       password: password,
-    });
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/login`,
+      captain,
+      {
+        withCredentials: true,
+      }
+    );
+
+    if (response.status === 200) {
+      const data = response.data;
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
+
     setEmail("");
     setPassword("");
   };
