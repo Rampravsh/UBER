b049ac734acdeb8ce981de6cf07b82621e659240
Rampravesh <rampraveshkr4545@gmail.com>
2026-04-19 16:43:48 +0530

    create more routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Start from "./pages/Start.jsx";
 import UserLogin from "./pages/UserLogin.jsx";
 import UserSignup from "./pages/UserSignup.jsx";
 import CaptianLogin from "./pages/CaptianLogin.jsx";
 import CaptainSignup from "./pages/CaptainSignup.jsx";
+import Home from "./pages/Home.jsx";
+import UserProtectWrapper from "./components/UserProtectWrapper.jsx";
+import UserLogout from "./pages/UserLogout.jsx";
+import CaptainHome from "./pages/CaptainHome.jsx";
 
 const App = () => {
   return (
     <div className="">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/user-signup" element={<UserSignup />} />
         <Route path="/captain-login" element={<CaptianLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
+        <Route path="/captain-home" element={<CaptainHome />} />
       </Routes>
     </div>
   );
