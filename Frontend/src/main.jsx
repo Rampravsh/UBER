4fd4a26eac9370e617cbe5a116fad6e0f497a2f2
Rampravesh <rampraveshkr4545@gmail.com>
2026-04-19 16:44:08 +0530

    update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
+import CaptainContext from "./context/CaptainContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <UserContext>
+    <CaptainContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CaptainContext>
+  </UserContext>,
 );
